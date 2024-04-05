59decabfa8e559fc83bb16dd43012ce5b925e16c
suaracost <alejandrosuarezacosta@gmail.com>
2024-04-05 11:04:59 -0500

    Cambio cosas del update

--- src/app/veterinario/vet-modificar-mascota/vet-modificar-mascota.component.ts
+++ src/app/veterinario/vet-modificar-mascota/vet-modificar-mascota.component.ts
@@ -1,53 +1,49 @@
-import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Mascota } from 'src/app/models/mascota';
 import { MascotaFakeService } from 'src/app/service/mascota-fake.service';
-declare var Swal: any;
 
 @Component({
   selector: 'app-vet-modificar-mascota',
   templateUrl: './vet-modificar-mascota.component.html',
-  styleUrls: ['./vet-modificar-mascota.component.css']
+  styleUrls: ['./vet-modificar-mascota.component.css'],
 })
 export class VetModificarMascotaComponent implements OnInit {
-
   listaMascotas!: Mascota[];
 
   constructor(
     private mascotaService: MascotaFakeService,
     private route: ActivatedRoute,
-    private router: Router,
-  ) {
-  }
+    private router: Router
+  ) {}
 
-  @Input() 
   mascotaAux!: Mascota;
 
   llegaMascota!: Mascota;
   formMascota!: Mascota;
- 
-  ngOnInit(): void {
 
-      this.route.paramMap.subscribe(params => {
-        const id = Number(params.get('id'));
-        this.mascotaAux = Object.assign({}, this.mascotaService.findById(id));
-        this.llegaMascota = Object.assign({}, this.mascotaAux);
-        this.formMascota = Object.assign({}, this.mascotaAux);
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const id = Number(params.get('id'));
+      this.mascotaAux = Object.assign({}, this.mascotaService.findById(id));
+      this.llegaMascota = Object.assign({}, this.mascotaAux);
+      this.formMascota = Object.assign({}, this.mascotaAux);
     });
+
     let sidebar = document.querySelector('.sidebar') as HTMLElement;
 
     sidebar.addEventListener('mouseover', () => {
-      sidebar.classList.add("active");
+      sidebar.classList.add('active');
     });
 
     sidebar.addEventListener('mouseleave', () => {
-      sidebar.classList.remove("active");
+      sidebar.classList.remove('active');
     });
   }
 
   sendMascota!: Mascota;
 
-  actualizarMascota(){
+  actualizarMascota() {
     this.sendMascota = Object.assign({}, this.formMascota);
     this.mascotaService.updateMascota(this.sendMascota);
     this.router.navigate(['/veterinario/mascotas/all']);
