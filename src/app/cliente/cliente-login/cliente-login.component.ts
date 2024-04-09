a587b746a47abac27127b2dc73d1619883d4732e
suaracost <alejandrosuarezacosta@gmail.com>
2024-04-09 11:07:04 -0500

    "Login" cliente funcional

--- src/app/cliente/cliente-login/cliente-login.component.ts
+++ src/app/cliente/cliente-login/cliente-login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Cliente } from 'src/app/models/cliente';
+import { ClienteServiceService } from 'src/app/service/cliente-service.service';
 
 @Component({
   selector: 'app-cliente-login',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./cliente-login.component.css']
 })
 export class ClienteLoginComponent {
+  
+  cedulaCliente: string = '';
+  listaClientes!: Cliente[];
+
+  constructor(
+    private clienteService: ClienteServiceService,
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this.clienteService.findAll().subscribe(
+      (clientes) => this.listaClientes = clientes
+    );
+  }
+
+  loginCliente() {
+    const clienteEncontrado = this.listaClientes.find(cliente => cliente.cedula === this.cedulaCliente);
+
+    if (clienteEncontrado) {
+      this.router.navigate(['/usuario/' + clienteEncontrado.id]);
+    } else {
+      //TODO: Llevar a pagina de error
+    }
+  }
 
 }
